c841a384e2ad3eec9740d69036b95f39e41e3980
XLMJAPAN <haryo0622apple@gmail.com>
2026-03-14 02:16:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get("/balance/:address", async (req, res) => {
 
   try {
 
+    // Stellarウォレット取得
     const response = await fetch(
       `https://horizon.stellar.org/accounts/${address}`
     );
@@ -22,11 +23,26 @@ app.get("/balance/:address", async (req, res) => {
       b => b.asset_type === "native"
     ).balance;
 
-    res.json({balance});
+    // XLM価格取得（JPY）
+    const priceRes = await fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=jpy"
+    );
+
+    const priceData = await priceRes.json();
+
+    const price = priceData.stellar.jpy;
+
+    const jpyValue = balance * price;
+
+    res.json({
+      balance,
+      price,
+      jpyValue
+    });
 
   } catch (error) {
 
-    res.json({error:"not found"});
+    res.json({ error: "not found" });
 
   }
 
