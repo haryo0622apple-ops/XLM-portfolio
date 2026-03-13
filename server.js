435a5b53d51f8e19dcfad10ec252f71fce9e467e
XLMJAPAN <haryo0622apple@gmail.com>
2026-03-14 02:12:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 const express = require("express");
 const fetch = require("node-fetch");
-const app = express();
 
-app.use(express.static("."));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
 
 app.get("/balance/:address", async (req, res) => {
+
   const address = req.params.address;
 
   try {
+
     const response = await fetch(
       `https://horizon.stellar.org/accounts/${address}`
     );
+
     const data = await response.json();
 
     const balance = data.balances.find(
-      (b) => b.asset_type === "native"
+      b => b.asset_type === "native"
     ).balance;
 
-    res.json({ balance });
+    res.json({balance});
 
   } catch (error) {
-    res.json({ error: "address not found" });
+
+    res.json({error:"not found"});
+
   }
+
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server running");
 });
