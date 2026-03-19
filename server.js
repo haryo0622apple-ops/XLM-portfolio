02ae39100e07e7eeab3ec1ab67271fe1e226313a
XLMJAPAN <haryo0622apple@gmail.com>
2026-03-20 02:19:50 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 1. 静的ファイル（index.html）を公開
+// index.htmlを公開
 app.use(express.static(path.join(__dirname, '.')));
 
-// 2. Stellarネットワークへの通信を中継（CORSエラー対策）
-// これにより、ブラウザは自分のサーバーに聞くだけで良くなります
+// Stellarネットワークへの通信を中継（CORSエラー対策）
 app.use('/stellar', proxy('https://stellar-mainnet.publicnode.com'));
 
 app.listen(PORT, () => {
