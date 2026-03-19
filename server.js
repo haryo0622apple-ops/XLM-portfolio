2dfe2f33310017fd13d8d0c735abc13bfdfa97f4
XLMJAPAN <haryo0622apple@gmail.com>
2026-03-20 02:44:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '.')));
-
-// ブラウザの代わりにStellarへデータを取りに行く中継口
-app.use('/stellar', proxy('https://stellar-mainnet.publicnode.com', {
-    proxyReqPathResolver: (req) => {
-        return req.url;
-    }
-}));
+app.use('/stellar', proxy('https://stellar-mainnet.publicnode.com'));
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
